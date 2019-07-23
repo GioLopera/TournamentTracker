08a7de09b9da087ee0aa4dade614a318ab8c6068
ifmadm <goupDown18>
2019-07-23 16:08:59 -0500

    Wired up SQL
    Adds connection to the data base
    wired up the SQL connection

--- TournamentTracker/TrackerLibrary/GlobalConfig.cs
+++ TournamentTracker/TrackerLibrary/GlobalConfig.cs
@@ -1,29 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TrackerLibrary.DataAccess;
 
 namespace TrackerLibrary
 {
     public static class GlobalConfig
     {
-        public static List<IDataConnection> Connections { get; private set; } = new List<IDataConnection>();
+        public static IDataConnection Connection { get; private set; }
 
-        public static void InitializeConnections(bool database, bool textfiles)
+        public static void InitializeConnections(DatabaseType db)
         {
-            if (database)
+
+            if (db == DatabaseType.Sql)
             {
-                //TODO - Set up SQL Connector properly
+               //TODO - Set up SQL Connector properly
                 SqlConnector sql = new SqlConnector();
-                Connections.Add(sql);
+                Connection = sql;
             }
-            if (textfiles)
+            else if (db == DatabaseType.TextFile)
             {
                 //TODO - Create textfile connection
                 TextConnector text = new TextConnector();
-                Connections.Add(text);
+                Connection = text;
             }
+        }
+
+        public static string CnnString(string name)
+        {
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
         }
     }
 }
